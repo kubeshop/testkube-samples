45574221b20e183d843c723717265b89252136d1
Delnat Wito <wito.delnat@gmail.com>
2024-05-13 20:35:53 +0200

    feat: fix 14

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1>Testkube + React v13</h1>
+        <h1>Testkube + React v14</h1>
       </div>
 
       <div className="w-full flex flex-col md:flex-row items-center gap-3">
